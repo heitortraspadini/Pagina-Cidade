b4d3daedeec0e96ffb217d8efaeab62bc0d4f6cb
Heitor Traspadini <159261533+heitortraspadini@users.noreply.github.com>
2024-07-31 09:21:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let imagens = document.getElementById('imagem')
+let legendas = document.getElementById('legenda')
+function imagem1() {
+    imagens.src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Xique-Xique_-_bandeira.jpg"
+    legendas.innerHTML="Bandeira de Xique-Xique"
+}
+function imagem2() {
+    imagens.src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Bahia_Municip_XiqueXique.svg/1024px-Bahia_Municip_XiqueXique.svg.png"
+    legendas.innerHTML='Xique-Xique no mapa da Bahia'
+}
+function imagem3() {
+    imagens.src="https://www.guiadoturismobrasil.com/up/img/1446417002.jpg"
+    legendas.innerHTML="Praça central de Xique-Xique"
+}
+function imagem4() {
+    imagens.src="https://www.xiquexique.ba.leg.br/imagens/XiqueXiqueSatelite.png/image_preview"
+    legendas.innerHTML='Vista panorâmica de Xique-Xique'
+}
